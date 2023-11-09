eff39bc04cf5a26f7485eea2b703c629c10e8fc8
tomhoyt <houyuteng0501@163.com>
2023-11-09 14:29:25 +0100

    recipepage initial created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeComponent } from './recipe/recipe.component';
-import {AppComponent} from "./app.component";
 
 const routes: Routes = [
-  { path: '', component: AppComponent},
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' }, // Redirect to /recipes
   { path: 'recipes', component: RecipeComponent },
+  // ... other routes
 ];
 
 @NgModule({
--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,10 +1,11 @@
+// recipe.component.ts
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-recipe',
-    templateUrl: 'recipe.component.html',
-    styleUrls: ['recipe.component.css']
+    templateUrl: './recipe.component.html',
+    styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent {
-    title = 'Recipes';
+    // RecipeComponent specific properties and methods
 }
