ca8b3096dc9a2987b0a3274c833f15dcfc9f1444
tomhoyt <houyuteng0501@163.com>
2023-11-09 14:07:44 +0100

    recipepage initial created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeComponent } from './recipe/recipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recipes', component: RecipeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module'; // Import AppRoutingModule
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component'; // Import RecipeComponent
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent
+    // ... any other components in your app
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule // Include it in the imports array
   ],
   providers: [],
   bootstrap: [AppComponent]
