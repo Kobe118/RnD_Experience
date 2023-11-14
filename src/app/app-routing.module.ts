2ec2f4d077c5e924abb50627f7518d5237cb83db
l-lisanne <101562139+l-lisanne@users.noreply.github.com>
2023-11-14 19:54:02 +0100

    added circular image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent} from './app.component';
-import {LogInComponent} from "./LogIn/LogIn.component"; // Update with the correct path to my-page.component
+import { FamiliesPageComponent} from "./families-page/families-page.component";
+import { LogInComponent} from "./LogIn/LogIn.component"; // Update with the correct path to my-page.component
 
 const routes: Routes = [
   { path: '', component: AppComponent }, // Use the correct component for the root path
   { path: 'LogIn', component: LogInComponent }, // Use the correct component for the second path
+  { path: 'families', component: FamiliesPageComponent}
 ];
 
 @NgModule({
--- src/app/default-header/default-header.component.ts
+++ src/app/default-header/default-header.component.ts
@@ -2,11 +2,8 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-default-header',
-  //templateUrl: './default-header.component.html',
+  templateUrl: './default-header.component.html',
   styleUrls: ['./default-header.component.scss'],
-  template: `<h1> {{title}}</h1>
-  <p class="flush-text">{{subtitle}}</p>
-  `
 })
 export class DefaultHeaderComponent {
     @Input() title="No title set yet";
