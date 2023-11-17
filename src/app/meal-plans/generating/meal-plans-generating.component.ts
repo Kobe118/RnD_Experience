4e7a31b80a9178d23c3eae967c0f19bebac3044f
Dante Kerstens <dante.kerstens@student.kuleuven.be>
2023-11-17 14:35:45 +0100

    Creation of the mealplan generate page

---
+++ src/app/meal-plans/generating/meal-plans-generating.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-meal-plans-generating',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './meal-plans-generating.component.html',
+  styleUrls: ['./meal-plans-generating.component.scss']
+})
+export class MealPlansGeneratingComponent {
+
+}
