f21d30ce50ba2a5b6fadb96b7881327c72982dd1
William <william.zhu@student.kuleuven.be>
2023-11-17 02:09:09 +0100

    app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LogINComponent } from './log-in/log-in.component';
-import { RegisterComponent } from './register/register.component';
+import { LogInComponent } from './log-in/log-in.component';
+
 import { DefaultHeaderComponent } from './default-header/default-header.component';
 import { FamiliesPageComponent } from './families-page/families-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
@@ -13,8 +13,6 @@ import { DietaryPreferencePageComponent } from './register/dietary-preference-pa
 import { SignUpPageComponent } from './register/sign-up-page/sign-up-page.component';
 import { CongratsPageComponent } from './register/congrats-page/congrats-page.component';
 import { AllergiesPageComponent } from './register/allergies-page/allergies-page.component';
-import { MealPlanHomeComponent } from './meal-plans/meal-plan-home/meal-plan-home.component';
-import { MealPlanCalendarComponent } from './meal-plans/meal-plan-calendar/meal-plan-calendar.component';
 import { MealPlansHomeComponent } from './meal-plans/meal-plans-home/meal-plans-home.component';
 import { MealPlansCalendarComponent } from './meal-plans/meal-plans-calendar/meal-plans-calendar.component';
 import { RecipeComponent } from './recipe/recipe.component';
@@ -23,8 +21,7 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LogINComponent,
-    RegisterComponent,
+    LogInComponent,
     DefaultHeaderComponent,
     FamiliesPageComponent,
     ProfilePageComponent,
@@ -33,8 +30,6 @@ import { HomeComponent } from './home/home.component';
     SignUpPageComponent,
     CongratsPageComponent,
     AllergiesPageComponent,
-    MealPlanHomeComponent,
-    MealPlanCalendarComponent,
     MealPlansHomeComponent,
     MealPlansCalendarComponent,
     RecipeComponent,
