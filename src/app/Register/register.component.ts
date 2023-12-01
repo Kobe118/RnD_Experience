98eda39e0ed1f0bd3b9b7c81225a5f77aa2b9238
DESKTOP-NSEAQDU\William <william.zhu@student.kuleuven.be>
2023-12-01 11:50:53 +0100

    register

--- src/app/Register/register.component.ts
+++ src/app/Register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core'
 import { FormBuilder } from '@angular/forms'
-import { SupabaseService} from "../services/supabase.service";
+import { SupabaseService } from "../services/supabase.service";
 
 @Component({
   selector: 'app-register',
