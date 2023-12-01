3c5c06736e833434d98600a0e2cec4bef94f01c6
DESKTOP-NSEAQDU\William <william.zhu@student.kuleuven.be>
2023-12-01 16:38:18 +0100

    bugfix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { MealPlansCalendarComponent }  from "./meal-plans/meal-plans-calendar/me
 import { RecipeComponent } from './recipe/recipe.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth-guard';
-
+ 
 
 const routes: Routes = [
   { path: 'login', component: LogInComponent, },
