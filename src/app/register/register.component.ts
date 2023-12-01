04e30cf346470ea3940b72a79560466f85662cff
williamzhu1 <75760014+williamzhu1@users.noreply.github.com>
2023-12-01 12:13:25 +0100

    Create register.component.ts

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+import { FormBuilder } from '@angular/forms'
+import { SupabaseService} from "../services/supabase.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './welcome/register.component.html',
+  styleUrls: ['./welcome/register.component.scss']
+})
+export class RegisterComponent {
+  
+}
