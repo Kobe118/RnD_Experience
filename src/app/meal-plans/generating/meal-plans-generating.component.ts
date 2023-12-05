9b086a567815b53300d27fdc51e14451b1b797f4
Dante Kerstens <dante.kerstens@student.kuleuven.be>
2023-12-05 12:24:07 +0100

    Meals plan generating component finished

--- src/app/meal-plans/generating/meal-plans-generating.component.ts
+++ src/app/meal-plans/generating/meal-plans-generating.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
+import {ModalGeneratingComponent} from "../modal-generating/modal-generating.component";
+import {SupabaseService} from "../../supabase.service";
+import {MdbModalRef} from "mdb-angular-ui-kit/modal";
 
 @Component({
   selector: 'app-meal-plans-generating',
@@ -9,5 +13,13 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./meal-plans-generating.component.scss']
 })
 export class MealPlansGeneratingComponent {
-
+  weekdays: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  modalRef: NgbModalRef | null = null;
+  constructor(private modalService: NgbModal, private supabaseService: SupabaseService) {
+  }
+  openModal(day: string) {
+    this.modalRef = this.modalService.open(ModalGeneratingComponent);
+    this.modalRef.componentInstance.day = day;
+    this.modalRef.componentInstance.modalRef = this.modalRef;
+  }
 }
