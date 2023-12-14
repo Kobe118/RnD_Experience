b1b20ff7f7400022dd9a4119071958f2a0658a17
l-lisanne <101562139+l-lisanne@users.noreply.github.com>
2023-12-14 22:05:04 +0100

    mealplan generating styling

--- src/app/meal-plans/generating/meal-plans-generating.component.ts
+++ src/app/meal-plans/generating/meal-plans-generating.component.ts
@@ -69,4 +69,8 @@ export class MealPlansGeneratingComponent implements OnInit {
     this.modalRef.componentInstance.family_id = this.family_id;
     this.modalRef.componentInstance.mealplan_id = this.mealplan_id;
   }
+
+  goBack(): void {
+    this.router.navigate(['/mealplanshome']);
+  }
 }
